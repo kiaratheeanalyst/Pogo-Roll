463f157af2dfc88ce0dd756910524f998b17f490
Kaylerr <itscookiebunny49@gmail.com>
2023-09-10 17:00:11 +0100

    init

---
+++ Pogo Roll/Assets/Scripts/LevelSelector.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelector : MonoBehaviour
+{
+	public void Select (string levelName)
+	{
+		SceneManager.LoadScene(levelName);
+	}
+
+	public void ToMenu()
+	{
+		SceneManager.LoadScene(0);
+	}
+}
